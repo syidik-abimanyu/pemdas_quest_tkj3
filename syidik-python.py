7a0350b1aa4402b8f70a3a758b36945374aab926
syidik-abimanyu <112368453+syidik-abimanyu@users.noreply.github.com>
2022-08-28 23:14:57 -0700

    Add files via upload

---
+++ syidik-python.py
@@ -0,0 +1,12 @@
+nama = input("siapa nama saya?")
+umur = input("berapa umur saya?")
+hobi = input("apa hobi kamu?")
+warna = input("apa warna kesukaan saya?")
+makanan = input("apa makanan kesukaan saya?")
+minuman = input("apa minuman kesukaan saya?")
+sekolah = input("nama sokolah saya?")
+kelas = input("kelas berapa saya?")
+kejurusan = input("apa jurusan saya?")
+saudara = input("berapa saudara saya?")
+
+print("nama sya",nama,"umur saya",umur,"hobi saya",hobi,"warna saya",warna,"makanan saya",makanan,"minuman saya",minuman,"sekolah saya",sekolah,"kelas saya",kelas,"kejurusan saya",kejurusan,"saudara saya",saudara)
