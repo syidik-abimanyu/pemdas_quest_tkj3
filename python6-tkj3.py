7d995c91804bbec5ce3adee8496fb4f346459a21
syidik-abimanyu <112368453+syidik-abimanyu@users.noreply.github.com>
2022-09-04 22:13:41 -0700

    Add files via upload

---
+++ python6-tkj3.py
@@ -0,0 +1,16 @@
+# progam dinamis
+
+#dolar-rupiah
+dolar = int(input("masukan nilai dolar:"))
+rupiah22 = dolar * 14973.8
+print("Nilai rupiah adalah",rupiah22)
+
+#euro-won
+euro = int(input("masukan nilai euro:"))
+won22 = euro * 1359.22
+print("Nilai won adalah",won22)
+
+#yen-rupiah
+yen = int(input("masukan nilai yen:"))
+rupiah22 = yen * 106
+print("Nilai rupiah adalah",rupiah22)
