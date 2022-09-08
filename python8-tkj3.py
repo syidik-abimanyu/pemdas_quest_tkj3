b9186085db2bc5a8c0a0702fa80e3398d6d28f54
syidik-abimanyu <112368453+syidik-abimanyu@users.noreply.github.com>
2022-09-07 23:37:19 -0700

    Add files via upload

---
+++ python8-tkj3.py
@@ -0,0 +1,15 @@
+list_kosong = []
+list_string = ["Syidik","Andre","Budi","Dapa"]
+#               0        1       2      3
+list_integer = [15,21,23,12]
+list_mix = ["syidik", 15, True, 98.0]
+
+print("\nDaftar nama nama peserta beserta tanggal lahir:")
+ 
+print("Nama berikut:", list_string[0], "Tanggal lahir", list_integer[0])
+
+print("Nama berikut:", list_string[1], "Tanggal lahir", list_integer[1])
+
+print("Nama berikut:", list_string[2], "Tanggal lahir", list_integer[2])
+
+print("Nama berikut:", list_string[3], "Tanggal lahir", list_integer[3])
