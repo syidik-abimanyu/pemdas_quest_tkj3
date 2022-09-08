6dcddbd9e2702df0a109d0976695ac5b5c93950c
syidik-abimanyu <112368453+syidik-abimanyu@users.noreply.github.com>
2022-09-07 23:36:55 -0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,42 @@
+'''Latihan'''
+
+# persegi
+
+print("\n1.Luas bangun persegi")
+sisi = int(input("masukan input sisi:"))
+luas_p = sisi * sisi
+print("Luas persegi adalah",luas_p)
+
+# persegi panjang
+
+print("\n2.Luas bangun persegi panjang")
+panjang = int(input("Masukan input panjang:"))
+lebar = int(input("Masukan input lebar:"))
+luas_pj = panjang * lebar
+print("Luas persegi panjang adalah",luas_pj)
+
+# segitiga
+
+print("\n3.Luas bangun segitiga")
+alas = int(input("Masukan input alas:"))
+tinggi = int(input("Masukan input tinggi:"))
+luas_s = alas * tinggi
+print("Luas segitiga adalah",luas_s)
+
+# lingkaran
+
+print("\n4.Luas bangun lingkaran")
+r = int(input("Masukan inout r:"))
+phi = 22/7
+luas_l = phi * r * r
+print("Luas lingkaran adalah",luas_l)
+
+# jajar genjang
+
+print("\n5.Luas bangun jajar genjang")
+alas = int(input("Masukan input alas:"))
+tinggi = int(input("Masukan input tinggi:"))
+luas_jg = alas * tinggi
+print("Luas jajar genjang adalah",luas_jg)
+
+
